a330079636c13d8d5ed52c09282daac80fafb50f
root <root@ip-172-31-34-202.ap-south-1.compute.internal>
2025-07-17 06:24:02 +0000

    Add appointment workflow and patient-service folder

---
+++ patient-service/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 80;
+const host = '0.0.0.0';
+
+app.get('/', (req, res) => {
+  res.send('✅ Welcome to Patient Service Root!');
+});
+
+app.get('/Patient', (req, res) => {
+  res.send('✅ Patient Service running on port !');
+});
+
+app.listen(port, host, () => {
+  console.log(`✅ Patient  service running on http://${host}:${port}`);
+});
